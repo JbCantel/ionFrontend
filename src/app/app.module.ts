a10528d828a31fb76c39cd7cce931f29d0e1258d
JbCantel <joaobcantel10@gmail.com>
2023-07-06 21:18:59 -0300

    formulário (incompleto)
    erros no FormControl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -11,10 +12,14 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { environment } from './../environments/environment';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
--- src/app/pages/contacts/contacts.page.ts
+++ src/app/pages/contacts/contacts.page.ts
@@ -1,4 +1,7 @@
+import { environment } from './../../../environments/environment';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contacts',
@@ -7,9 +10,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactsPage implements OnInit {
 
-  constructor() { }
+  public env = environment;
+  contactForm!: FormGroup;
+  contact!: any;
+  success: boolean = false;
+  firstName: String = "Visitante";
 
-  ngOnInit() {
+  validationMessages: any = {
+    name: {
+      required: 'O nome é obrigatório.'
+    },
+    email: {
+      required: 'O email é obrigatório.',
+      email: 'Digite um email válido.'
+    },
+    subject: {
+      required: 'O assunto é obrigatório.'
+    },
+    message: {
+      required: 'A mensagem é obrigatória.'
+    }
   }
+
+  formErrors: any = {
+    name: '',
+    email: '',
+    subject: '',
+    message: ''
+  }
+
+  constructor(
+    // Injeção de dependências.
+    private formBuilder: FormBuilder,
+    private http: HttpClient
+  ) { }
+
+  ngOnInit(): void {
+    this.createForm();
+    this.success = false;
+
+    this.contactForm.valueChanges.subscribe(() => {
+      // this.updateValidationMessages();
+    });
+  }
+
+  createForm() {
+    this.contactForm = this.formBuilder.group({
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3)
+        ]
+      ],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email
+        ]
+      ],
+      subject: ['', [Validators.required, Validators.minLength(3)]],
+      message: ['', [Validators.required, Validators.minLength(5)]]
+    });
+  }
+
+  updateValidationMessages() {
+    for (const field in this.formErrors) {
+      if (Object.prototype.hasOwnProperty.call(this.formErrors, field)) {
+        this.formErrors[field] = '';
+        const control = this.contactForm.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (Object.prototype.hasOwnProperty.call(control.errors, key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
+  }
+
 
 }
